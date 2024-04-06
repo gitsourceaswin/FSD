0ea878dfcda1156ad29fcc6c8207d1f16536db57
Aswin U <107989429+gitsourceaswin@users.noreply.github.com>
2024-04-06 12:08:21 +0530

    apr_6_3

--- Students/stureact/src/Fee.js
+++ Students/stureact/src/Fee.js
@@ -1,11 +1,55 @@
 // Fee.js
-import React from 'react';
+import React, { useState } from 'react';
+import './Fee.css';
 
 function Fee() {
+  const [activeAccordion, setActiveAccordion] = useState('');
+
+  const toggleAccordion = (accordionId) => {
+    if (activeAccordion === accordionId) {
+      setActiveAccordion('');
+    } else {
+      setActiveAccordion(accordionId);
+    }
+  };
+
   return (
-    <div>
-      <h1>Fee</h1>
-      <p>Welcome to the Fee page!</p>
+    <div className='Fee'>
+      <div
+        className={`accordion ${activeAccordion === 'Full_Time' ? 'active' : ''}`}
+        onClick={() => toggleAccordion('Full_Time')}
+      >
+       FEE
+      </div>
+      <div
+        id='Full_Time'
+        className={`tabcontent ${activeAccordion === 'Full_Time' ? 'show' : ''}`}
+      >
+        <table>
+          <tbody>
+            <tr>
+                <th></th>
+                <th><center>Fee Details</center></th>
+                <th></th>
+            </tr> {/* Added the closing tag here */}
+            <tr>
+                <th>Particulars</th>
+                <th>Actual Fee</th>
+                <th>Applicable Fee</th>
+            </tr>
+            <tr>
+                <td>Tution Fee</td>
+                <td>87000</td>
+                <td>77000</td>
+            </tr>
+            <tr>
+                <td>Exam Fee</td>
+                <td>5400</td>
+                <td>5400</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- Students/stureact/src/Placements.js
+++ Students/stureact/src/Placements.js
@@ -1,11 +1,99 @@
 // Placements.js
-import React from 'react';
+import React, { useState } from 'react';
+import './Placements.css';
 
 function Placements() {
+  const [activeAccordion, setActiveAccordion] = useState('');
+
+  const toggleAccordion = (accordionId) => {
+    if (activeAccordion === accordionId) {
+      setActiveAccordion('');
+    } else {
+      setActiveAccordion(accordionId);
+    }
+  };
+
   return (
-    <div>
-      <h1>Placements</h1>
-      <p>Welcome to the Placements page!</p>
+    <div className='Placements'>
+      <div
+        className={`accordion ${activeAccordion === 'Full_Time' ? 'active' : ''}`}
+        onClick={() => toggleAccordion('studentDetails')}
+      >
+        Full Time
+      </div>
+      <div
+        id='Full_Time'
+        className={`tabcontent ${activeAccordion === 'Full_Time' ? 'show' : ''}`}
+      >
+        <table>
+          <tbody>
+          <tr>
+                    <th>Company Name</th>
+                    <th>Package</th>
+                    <th>Date of Joining</th>
+                </tr>
+                <tr>
+                    <td>Google</td>
+                    <td>24.5 LPA</td>
+                    <td>01.03.2024</td>
+                </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div
+        className={`accordion ${activeAccordion === 'Internship' ? 'active' : ''}`}
+        onClick={() => toggleAccordion('Internship')}
+      >
+        Internship
+      </div>
+      <div
+        id='Internship'
+        className={`tabcontent ${activeAccordion === 'Internship' ? 'show' : ''}`}
+      >
+        <table>
+          <tbody>
+          <tr>
+                    <th>Company Name</th>
+                    <th>Package</th>
+                    <th>Date of Joining</th>
+                </tr>
+                <tr>
+                    <td>Google</td>
+                    <td>24.5 LPA</td>
+                    <td>01.03.2024</td>
+                </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div
+        className={`accordion ${activeAccordion === 'Research' ? 'active' : ''}`}
+        onClick={() => toggleAccordion('Research')}
+      >
+        Research
+      </div>
+      <div
+        id='Transcript'
+        className={`tabcontent ${activeAccordion === 'Research' ? 'show' : ''}`}
+      >
+        <table>
+          <tbody>
+                <tr>
+                    <th>Tag Name</th>
+                    <th>Project</th>
+                    <th>Guide</th>
+                </tr>
+                <tr>
+                    <td>SCN</td>
+                    <td>Project Name</td>
+                    <td>TSK</td>
+                </tr>
+          </tbody>
+        </table>
+      </div>
+
+
     </div>
   );
 }
