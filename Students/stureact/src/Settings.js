2e934f412413588af8fcbdfb2f46b767375aa790
Aswin U <107989429+gitsourceaswin@users.noreply.github.com>
2024-04-16 11:29:39 +0530

    Apr16_1

--- Students/stureact/src/Settings.js
+++ Students/stureact/src/Settings.js
@@ -1,11 +1,48 @@
-// Settings.js
-import React from 'react';
+import React, { useState } from 'react';
 
 function Settings() {
+  const [activeAccordion, setActiveAccordion] = useState('');
+  const toggleAccordion = (accordionId) => {
+    setActiveAccordion(activeAccordion === accordionId ? '' : accordionId);
+  };
+
+ 
   return (
-    <div>
-      <h1>Settings</h1>
-      <p>Welcome to the Settings page!</p>
+    <div className='HelpDesk'>
+      <div
+        className={`accordion ${activeAccordion === 'Submit_New' ? 'active' : ''}`}
+        onClick={() => toggleAccordion('Submit_New')}
+      >
+        Change password
+      </div>
+      <div
+        id='Submit_New'
+        className={`tabcontent ${activeAccordion === 'Submit_New' ? 'show' : ''}`}
+      >
+        <form>
+          <table>
+            <tbody>
+              <tr>
+                <th>Current Password</th>
+                <td><input type='text' placeholder='Current Password'/></td>
+              </tr>
+              <tr>
+                <th>New Password</th>
+                <td><input type='text' placeholder='New Password' /></td>
+              </tr>
+              <tr>
+                <th>Confirm Password</th>
+                <td><input type='text' placeholder='Confirm Password' /></td>
+              </tr>
+              <tr>
+                <td colSpan='2'>
+                  <button type='submit'>Submit</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </div>
     </div>
   );
 }
